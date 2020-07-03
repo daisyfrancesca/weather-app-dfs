6986c21da7cc674d79a9a5eea579d952f5d7711e
daisyfrancesca <65689227+daisyfrancesca@users.noreply.github.com>
2020-07-03 14:26:49 -0400

    Added JavaScript format, added current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `Wind: ${Math.round(
+    response.data.wind.speed * 2.237
+  )} mph`;
+}
 
+let apiKey = "d643ee59f43b44ad31e57464532264d8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
